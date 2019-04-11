a3442c14739f24ec64c41bcfe7665909f1580c51
Patryk <patrykmaruda@gmail.com>
2019-04-11 12:08:27 +0200

    Add some

--- webapp/main/controllers.py
+++
@@ -1,8 +0,0 @@
-
-from flask import Blueprint , render_template , redirect, url_for
-
-main_blueprint = Blueprint('main' , __name__)
-
-@main_blueprint.route('/test')
-def index():
-    return 'You are in index'
